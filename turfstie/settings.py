46e34689e81f1be177eb229d70b251216af5321f
shriraamsj21 <shriraamsj21@gmail.com>
2026-07-23 13:36:12 +0530

    Update settings.py

--- turfstie/settings.py
+++ turfstie/settings.py
@@ -132,6 +132,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
